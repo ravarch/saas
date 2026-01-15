549978820acb7a88b78b57d5fc85049e3faa07f9
ravarch <rav2147333@maricopa.edu>
2026-01-16 02:34:53 +0600

    Delete drizzle.config.ts

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  schema: "./src/db/schema.ts",
-  out: "./migrations",
-  dialect: "sqlite",
-  driver: "d1-http",
-  tablesFilter: ["!_cf_KV"], // Ignore internal CF tables
-});
