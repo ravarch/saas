c82bea2142e9aa2c6ad8e0b5356453d9e36071bb
ravarch <rav2147333@maricopa.edu>
2026-01-16 02:32:21 +0600

    Create drizzle.config.ts

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./migrations",
+  dialect: "sqlite",
+  driver: "d1-http",
+  tablesFilter: ["!_cf_KV"], // Ignore internal CF tables
+});
